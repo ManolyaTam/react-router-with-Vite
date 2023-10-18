19dca48833f9b8cfb6c576e33f2db0e8a06387cd
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-18 11:31:38 +0300

    loading contacts and creating new empty ones

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './routes/root'
 import ErrorPage from './error-page'
 import './index.css'
 import Contact from "./routes/contacts.jsx";
-
+import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+import { loader as contactLoader } from './routes/contacts.jsx'
 
 const router = createBrowserRouter([{
   path: '/',
   element: <Root />,
   errorElement: <ErrorPage />,
+  loader: rootLoader,
+  action: rootAction,
   children: [
     {
       path: "contacts/:contactId",
       element: <Contact />,
+      loader: contactLoader
     }
   ]
 },
