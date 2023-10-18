cde581f2fdf6c03bc19600aa6410a5141a9c91cc
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-18 11:17:06 +0300

    adding contact route UI

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './routes/root'
 import ErrorPage from './error-page'
 import './index.css'
+import Contact from "./routes/contacts.jsx";
+
 
 const router = createBrowserRouter([{
   path: '/',
   element: <Root />,
-  errorElement: <ErrorPage />
-}])
+  errorElement: <ErrorPage />,
+  children: [
+    {
+      path: "contacts/:contactId",
+      element: <Contact />,
+    }
+  ]
+},
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
